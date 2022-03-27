f1857dab38acc3790a9db2b72acf5106d866fbb7
Mark333s <markomelnychyn333@gmail.com>
2022-03-27 20:33:15 +0000

    result

--- navigation/src/Choose/Choose.js
+++ navigation/src/Choose/Choose.js
@@ -22,22 +22,49 @@ const Choose = ()=>{
                 break
        }
    }
+
+   const emailHandler =(e)=>{
+       setEmail(e.target.value)
+       const re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+       
+       if(!re.test(String(e.target.value).toLowerCase())){
+           setEmailError('неправильний Email')
+       }else{
+           setEmailError('')
+       }
+   }
+
+   const passwordHandler = (e)=>{
+       setPassword(e.target.value)
+       if(e.target.value.length < 3 || e.target.value.length > 8){
+           setPasswordError('s')
+    }if (!e.target.value){
+        setEmailError('неправильний Email')
+    }
+       else {
+           setPasswordError('')
+       }
+   }
    
     return(
         <div>
             <form className={s.frm}>
             <h1> Registration</h1>
-            {(emailDirty && emailError) && <div style={{color: 'red'}}>{emailError}</div>}
+            {(emailDirty && emailError) && <div className={s.err}>{emailError}</div>}
             <input    
+            onChange={e=> emailHandler(e)}
+            value={email}
             onBlur={e=> blurHandler(e)}
             className={s.f} 
             name="email" 
             type='text' 
             placeholder="type your Email..." />
            
-            {(passwordDirty && passwordError) && <div style={{color: 'red'}}>{passwordError}</div>}
+            {(passwordDirty && passwordError) && <div className={s.er}>{passwordError}</div>}
             
             <input 
+             onChange={e=> passwordHandler(e)}
+            value={password}
             onBlur={e=> blurHandler(e)}
             className ={s.s} 
             name="password" 
