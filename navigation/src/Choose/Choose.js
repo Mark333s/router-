182237a44e4b278952c05f3b1ad1dac2698dbb91
Mark333s <markomelnychyn333@gmail.com>
2022-03-27 14:13:31 +0000

    s

--- navigation/src/Choose/Choose.js
+++ navigation/src/Choose/Choose.js
@@ -1,15 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import s from './Choose.module.css'
 
 
 const Choose = ()=>{
+   
+   const [email , setEmail] = useState('')
+   const [password , setPassword] = useState('')
+   const [emailDirty , setEmailDirty] = useState(false)
+   const [passwordDirty , setPasswordDirty] = useState(false)
+   const [emailError , setEmailError] = useState('Email не може бути пустим')
+   const [passwordError , setPasswordError] = useState('password не може бути пустим')
+
+
+
+   const blurHandler = (e) => {
+       switch (e.targe.name) {
+           case 'email':
+                setEmailDirty(true)
+           case 'password':
+                setPasswordDirty(true)
+                break
+       }
+   }
+   
     return(
         <div>
             <form className={s.frm}>
             <h1> Registration</h1>
-            <input name="email" type='text' placeholder="type your Email..." />
-            <input name="password" type='password' placeholder="Enter your password..." />
-            <button type='submit'>Registration</button>
+            {(emailDirty && emailError) && <div style={{color: 'red'}}>{emailError}</div>}
+            <input    
+            onBlur={e=> blurHandler(e)}
+            className={s.f} 
+            name="email" 
+            type='text' 
+            placeholder="type your Email..." />
+           
+            {(passwordDirty && passwordError) && <div style={{color: 'red'}}>{passwordError}</div>}
+            
+            <input 
+            onBlur={e=> blurHandler(e)}
+            className ={s.s} 
+            name="password" 
+            type='password' 
+            placeholder="Enter your password..." />
+            
+            <button className ={s.b} type='submit'>Registration</button>
             </form>
            
 
